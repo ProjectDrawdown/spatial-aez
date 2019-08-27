4ec668e9760cba56fb17b0a5e5da9c12455ab75e
Denton Gentry <dgentry@carboncaptu.re>
2019-08-27 04:25:20 -0700

    Add tests.

---
+++ test_extract_country_data.py
@@ -0,0 +1,19 @@
+import os.path
+import pytest
+import tempfile
+
+import extract_country_data as ecd
+
+def test_admin_lookup():
+    assert ecd.admin_lookup('Cabo Verde') == 'Cape Verde'
+    assert ecd.admin_lookup('Scarborough Reef') is None
+
+def test_generate_csv():
+    tmpdirobj = tempfile.TemporaryDirectory()
+    shapefilename = 'ne_10m_admin_0_countries/ne_10m_admin_0_countries.shp'
+    worldmapname = 'Beck_KG_V1/Beck_KG_V1_present_0p5.tif'
+    csvfilename = os.path.join(tmpdirobj.name, 'test.csv')
+    assert not os.path.exists(csvfilename)
+    ecd.main(shapefilename=shapefilename, worldmapname=worldmapname, tmpdir=tmpdirobj.name,
+            csvfilename=csvfilename)
+    assert os.path.exists(csvfilename)
