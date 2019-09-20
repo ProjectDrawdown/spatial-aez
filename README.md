cbb747adac06b9331a438d67de6a72485e2d4d62
Denton Gentry <dgentry@carboncaptu.re>
2019-09-20 10:54:49 -0700

    First steps toward Land Cover processing.
    1. Move data files and directories into data/ subdirectory.
    2. Generalize map processing, pass in a lookup object
       to allow the same code to process Köppen-Geiger and Land
       Cover GeoTIFF files.
    3. Add ESACCI-LC-L4-LCCS-Map-300m-P1Y-2015-v2.0.7.tif from
       https://storage.googleapis.com/cci-lc-v207/ESACCI-LC-L4-LCCS-Map-300m-P1Y-2015-v2.0.7.zip
       (as linked from http://maps.elie.ucl.ac.be/CCI/viewer/download.php)
    4. move country name lookup into a separate file.
    5. Make it possible to drop the code into pdb by sending it SIGUSR1,
       helpful for debugging.

--- README.md
+++ README.md
@@ -1,13 +1,24 @@
-## Process Köppen-Geiger climate classification maps ##
+## Process spatial climate maps
 
-Code in this repository processes the data files from the Nature article
-["Present and future Köppen-Geiger climate classification maps at 1-km resolution"](https://www.nature.com/articles/sdata2018214.pdf)
+Code in this repository extracts land area data from spatial maps for use in climate solution models.
 
-[Paper's data files](http://www.gloh2o.org/koppen/) retrieved 8/22/2019.
-[Natural Earth topology files](https://www.naturalearthdata.com/downloads/) retrieved 8/22/2019.
-
-The output of this code is a pair of CSV files for present and future, with a
-column per Köppen-Geiger class and row per country. Names are normalized to
+The output of this code is a series of CSV files for present and future, with
+a row per country and column per data class.  Names are normalized to
 those used in [Project Drawdown](https://drawdown.org). Square kilometers per
 [Köppen-Geiger class](https://en.wikipedia.org/wiki/K%C3%B6ppen_climate_classification)
 are calculated per country, correcting for WGS84 projection.
+
+
+### Köppen-Geiger
+
+Data files come from the Nature article
+["Present and future Köppen-Geiger climate classification maps at 1-km resolution"](https://www.nature.com/articles/sdata2018214.pdf).
+
+[Paper's data files](http://www.gloh2o.org/koppen/) retrieved 22 Aug 2019.
+[Natural Earth topology files](https://www.naturalearthdata.com/downloads/) retrieved 22 Aug 2019.
+
+
+### Land Cover
+Data comes from the [European Space Agency Climate Change Initiative](http://maps.elie.ucl.ac.be/CCI/viewer/download.php).
+
+[Land Cover GeoTIFF](https://storage.googleapis.com/cci-lc-v207/ESACCI-LC-L4-LCCS-Map-300m-P1Y-2015-v2.0.7.zip) retrieved 19 Sep 2019.
--- extract_country_data.py
+++ extract_country_data.py
@@ -2,13 +2,18 @@
 import collections
 import math
 import os.path
+import pdb
+import signal
+import sys
 import tempfile
 
 import osgeo.gdal
 import osgeo.gdal_array
 import osgeo.ogr
-
 import pandas as pd
+
+import admin_names
+
 
 pd.set_option("display.max_rows", 500)
 pd.set_option("display.max_columns", 40)
@@ -17,41 +22,81 @@ osgeo.gdal.PushErrorHandler("CPLQuietErrorHandler")
 
 
 
-# Lookup table of pixel color to Köppen-Geiger class.
-#
-# Mappings come from legend.txt file in ZIP archive from
-# https://www.nature.com/articles/sdata2018214.pdf at http://www.gloh2o.org/koppen/
-kg_colors = {
-    (  0,   0, 255): 'Af',  (  0, 120, 255): 'Am',  ( 70, 170, 250): 'Aw',
-    (255,   0,   0): 'BWh', (255, 150, 150): 'BWk', (245, 165,   0): 'BSh',
-    (255, 220, 100): 'BSk',
-    (255, 255,   0): 'Csa', (200, 200,   0): 'Csb', (150, 150,   0): 'Csc',
-    (150, 255, 150): 'Cwa', (100, 200, 100): 'Cwb', ( 50, 150,  50): 'Cwc',
-    (200, 255,  80): 'Cfa', (100, 255,  80): 'Cfb', ( 50, 200,   0): 'Cfc',
-    (255,   0, 255): 'Dsa', (200,   0, 200): 'Dsb', (150,  50, 150): 'Dsc',
-    (150, 100, 150): 'Dsd', (170, 175, 255): 'Dwa', ( 90, 120, 220): 'Dwb',
-    ( 75,  80, 180): 'Dwc', ( 50,   0, 135): 'Dwd', (  0, 255, 255): 'Dfa',
-    ( 55, 200, 255): 'Dfb', (  0, 125, 125): 'Dfc', (  0,  70,  95): 'Dfd',
-    (178, 178, 178): 'ET',  (102, 102, 102): 'EF',
-    }
+class KGlookup:
+    """Lookup table of pixel color to Köppen-Geiger class.
+
+       Mappings come from legend.txt file in ZIP archive from
+       https://www.nature.com/articles/sdata2018214.pdf at http://www.gloh2o.org/koppen/
+    """
+    kg_colors = {
+        (  0,   0, 255): 'Af',  (  0, 120, 255): 'Am',  ( 70, 170, 250): 'Aw',
+        (255,   0,   0): 'BWh', (255, 150, 150): 'BWk', (245, 165,   0): 'BSh',
+        (255, 220, 100): 'BSk',
+        (255, 255,   0): 'Csa', (200, 200,   0): 'Csb', (150, 150,   0): 'Csc',
+        (150, 255, 150): 'Cwa', (100, 200, 100): 'Cwb', ( 50, 150,  50): 'Cwc',
+        (200, 255,  80): 'Cfa', (100, 255,  80): 'Cfb', ( 50, 200,   0): 'Cfc',
+        (255,   0, 255): 'Dsa', (200,   0, 200): 'Dsb', (150,  50, 150): 'Dsc',
+        (150, 100, 150): 'Dsd', (170, 175, 255): 'Dwa', ( 90, 120, 220): 'Dwb',
+        ( 75,  80, 180): 'Dwc', ( 50,   0, 135): 'Dwd', (  0, 255, 255): 'Dfa',
+        ( 55, 200, 255): 'Dfb', (  0, 125, 125): 'Dfc', (  0,  70,  95): 'Dfd',
+        (178, 178, 178): 'ET',  (102, 102, 102): 'EF',
+        }
+
+    def __init__(self, ctable):
+        self.ctable = ctable
+
+    def get_index(self, label):
+        r, g, b, a = ctable.GetColorEntry(int(label))
+        color = (r, g, b)
+        if color == (255, 255, 255):
+            # blank pixel == masked off, just skip it.
+            return None
+        return self.kg_colors[color]
+
+    def get_columns(self):
+        return self.kg_colors.values()
 
 
-def rasterize_shapefile(worldimage, shpfile, outfile):
-    """Rasterize a shapefile to TIFF."""
-    driver = osgeo.ogr.GetDriverByName("ESRI Shapefile")
-    data_source = driver.Open(shpfile, 0)
-    layer = data_source.GetLayer()
-    datatype = osgeo.gdal.GDT_Byte
-    output = osgeo.gdal.GetDriverByName('GTiff').Create(outfile, worldimage.RasterXSize,
-            worldimage.RasterYSize, 1, datatype, options=['COMPRESS=DEFLATE'])
-    output.SetProjection(worldimage.GetProjectionRef())
-    output.SetGeoTransform(worldimage.GetGeoTransform()) 
-    band = output.GetRasterBand(1)
-    band.SetNoDataValue(0)
-    osgeo.gdal.RasterizeLayer(output, [1], layer, options=['ATTRIBUTE=ADMIN'])
+class LClookup:
+    """Pixel color to Land Cover class in ESACCI-LC-L4-LCCS-Map-300m-P1Y-2015-v2.0.7.tif
+
+       There are legends of LCCS<->color swatch in both
+       http://maps.elie.ucl.ac.be/CCI/viewer/download/ESACCI-LC-QuickUserGuide-LC-Maps_v2-0-7.pdf
+       and section 9.1 of http://maps.elie.ucl.ac.be/CCI/viewer/download/ESACCI-LC-Ph2-PUGv2_2.0.pdf
+       but these were generated from Microsoft Word documents and contain embedded color profiles
+       for color correction, meaning that what is displayed on the screen (and to any sort of
+       Digital Color Meter tool) have been shifted and do not match the original colors. Do not
+       attempt to use the color legend in these files.
+
+       There is a table of RGB to LCCS values in
+       http://maps.elie.ucl.ac.be/CCI/viewer/download/ESACCI-LC-Legend.csv
+       however: the GeoTIFF file as shipped is greyscale with no color table. Each pixel is 8 bits
+       not because it is looked up in a color table, it is 8 bits because it has been converted to
+       greyscale. The authors appear to have carefully chosen the RGB values such that when
+       converted to greyscale, LCCS class 10 will have a grey value of 10, LCCS class 11 will have
+       a grey value of 11, and so on.
+
+       So we don't need a lookup table, greyscale absolute values directly equal the LCCS class."""
+
+    def get_index(self, label):
+        if label == 0:
+            # black == no land cover (like water), just skip it.
+            return None
+        return label
+
+    def get_columns(self):
+        """Return list of LCCS classes present in this dataset."""
+        return [10, 11, 12, 20, 30, 40, 50, 60, 61, 62, 70, 71, 72, 80, 81, 82, 90, 100, 110,
+                120, 121, 122, 130, 140, 150, 151, 152, 153, 160, 170, 180, 190, 200, 201, 202,
+                210, 220]
 
 
-def one_feature_shapefile(worldmapname, worldimage, a3, idx, feature, tmpdir, srs):
+def start_pdb(sig, frame):
+    """Start PDB on a signal."""
+    pdb.Pdb().set_trace(frame)
+
+
+def one_feature_shapefile(mapfilename, a3, idx, feature, tmpdir, srs):
     """Make a new shapefile, to hold the one Feature we're looking at."""
     driver = osgeo.ogr.GetDriverByName("ESRI Shapefile")
     shpfile = os.path.join(tmpdir, f'{a3}_{idx}_feature_mask.shp')
@@ -67,20 +112,16 @@ def one_feature_shapefile(worldmapname, worldimage, a3, idx, feature, tmpdir, sr
     data_source = None
     layer = None
 
-    # Rasterise the shapefile just created. We don't strictly need this, but useful for debugging.
-    outfile = os.path.join(tmpdir, f'{a3}_{idx}_feature_mask.tif')
-    rasterize_shapefile(worldimage=worldimage, shpfile=shpfile, outfile=outfile)
-
     # Apply shapefile as a mask, and crop to the size of the mask
     clippedfile = os.path.join(tmpdir, f'{a3}_{idx}_feature.tif')
-    result = osgeo.gdal.Warp(clippedfile, worldmapname, cutlineDSName=shpfile, cropToCutline=True,
+    result = osgeo.gdal.Warp(clippedfile, mapfilename, cutlineDSName=shpfile, cropToCutline=True,
             warpOptions = ['CUTLINE_ALL_TOUCHED=TRUE'])
     if result is not None:
         return clippedfile
 
 
-def update_df_from_image(filename, admin, ctable, df):
-    """Count K-G classes by pixel, add to df."""
+def update_df_from_image(filename, admin, lookupobj, df):
+    """Count classes by pixel, add to df."""
     img = osgeo.gdal.Open(filename, osgeo.gdal.GA_ReadOnly)
     xmin, xsiz, xrot, ymin, yrot, ysiz = img.GetGeoTransform()
     img = None
@@ -92,120 +133,40 @@ def update_df_from_image(filename, admin, ctable, df):
         xlen = abs(xsiz) * (math.cos(y) * math.pi * 6378.137 /
                 (180 * math.sqrt(1 - 0.00669437999014 * (math.sin(y) ** 2))))
         # https://en.wikipedia.org/wiki/Latitude#Length_of_a_degree_of_latitude
-        ylen = abs(ysiz) * (111.132954 - (0.559822 * math.cos(2 * y)) + 
+        ylen = abs(ysiz) * (111.132954 - (0.559822 * math.cos(2 * y)) +
                 (0.001175 * math.cos(4 * y)))
         km2 = xlen * ylen
         counts = collections.Counter(row)
         for (label, count) in counts.items():
-            r, g, b, a = ctable.GetColorEntry(int(label))
-            color = (r, g, b)
-            if color == (255, 255, 255):
-                # blank pixel == masked off, just skip it.
+            idx = lookupobj.get_index(label)
+            if idx is None:
                 continue
-            kg_class = kg_colors[color]
-            df.loc[admin, kg_class] += (count * km2)
+            df.loc[admin, idx] += (count * km2)
         y -= yrad
 
 
-def admin_lookup(admin):
-    """Map Natural Earth ADMIN names to Drawdown country names."""
-    adminmap = {
-            'Akrotiri Sovereign Base Area': 'United Kingdom',
-            'Aland': 'Finland',
-            'American Samoa': 'United States of America',
-            'Anguilla': 'United Kingdom',
-            'Aruba': 'Netherlands',
-            'Ashmore and Cartier Islands': 'Australia',
-            'The Bahamas': 'Bahamas',
-            'Bajo Nuevo Bank (Petrel Is.)': None,
-            'Baykonur Cosmodrome': 'Kazakhstan',
-            'British Indian Ocean Territory': 'United Kingdom',
-            'British Virgin Islands': 'United Kingdom',
-            'Bermuda': 'United Kingdom',
-            'Cabo Verde': 'Cape Verde',
-            'Cayman Islands': 'United Kingdom',
-            'Clipperton Island': 'France',
-            'Republic of the Congo': 'Congo',
-            'Coral Sea Islands': 'Australia',
-            'Cyprus No Mans Area': 'Cyprus',
-            'Czechia': 'Czech Republic',
-            'Dhekelia Sovereign Base Area': 'United Kingdom',
-            'East Timor': 'Timor-Leste',
-            'Falkland Islands': None,
-            'Federated States of Micronesia': 'Micronesia (Federated States of)',
-            'French Polynesia': 'France',
-            'French Southern and Antarctic Lands': 'France',
-            'Gibraltar': 'United Kingdom',
-            'Guam': 'United States of America',
-            'Guernsey': 'United Kingdom',
-            'Heard Island and McDonald Islands': 'Australia',
-            'Hong Kong S.A.R.': 'Hong Kong',
-            'Indian Ocean Territories': None,
-            'Isle of Man': None,
-            'Ivory Coast': "Côte d'Ivoire",
-            'Jersey': 'United Kingdom',
-            'North Korea': "Democratic People's Republic of Korea",
-            'Kosovo': None,
-            'Laos': "Lao People's Democratic Republic",
-            'Macedonia': 'The former Yugoslav Republic of Macedonia',
-            'Macao S.A.R': None,
-            'Montserrat': 'United Kingdom',
-            'New Caledonia': 'France',
-            'Northern Cyprus': 'Cyprus',
-            'Norfolk Island': 'Australia',
-            'Northern Mariana Islands': 'United States of America',
-            'Pitcairn Islands': 'United Kingdom',
-            'Puerto Rico': 'United States of America',
-            'Republic of Serbia': 'Serbia',
-            'Russia': 'Russian Federation',
-            'Saint Barthelemy': 'France',
-            'Saint Helena': 'United Kingdom',
-            'Saint Martin': None,
-            'Saint Pierre and Miquelon': 'France',
-            'Scarborough Reef': None,
-            'Serranilla Bank': None,
-            'Siachen Glacier': None,
-            'Sint Maarten': None,
-            'Somaliland': 'Somalia',
-            'South Georgia and the Islands': 'United Kingdom',
-            'South Korea': 'Republic of Korea (South Korea)',
-            'Spratly Islands': None,
-            'Syria': 'Syrian Arab Republic',
-            'Turks and Caicos Islands': 'United Kingdom',
-            'United States Minor Outlying Islands': 'United States of America',
-            'United States Virgin Islands': 'United States of America',
-            'US Naval Base Guantanamo Bay': 'United States of America',
-            'Vatican': 'Holy See',
-            'Wallis and Futuna': 'France',
-            'eSwatini': 'Swaziland',
-            }
-    return adminmap.get(admin, admin)
-
-
-def main(shapefilename, worldmapname, tmpdir, csvfilename):
-    df = pd.DataFrame(columns=kg_colors.values(), dtype=float)
+def process_map(shapefilename, mapfilename, lookupobj, csvfilename):
+    tmpdirobj = tempfile.TemporaryDirectory()
+    df = pd.DataFrame(columns=lookupobj.get_columns(), dtype=float)
     df.index.name = 'Country'
     shapefile = osgeo.ogr.Open(shapefilename)
     assert shapefile.GetLayerCount() == 1
     layer = shapefile.GetLayerByIndex(0)
     srs = layer.GetSpatialRef()
-    worldimage = osgeo.gdal.Open(worldmapname, osgeo.gdal.GA_ReadOnly)
-    ctable = worldimage.GetRasterBand(1).GetColorTable()
 
     for idx, feature in enumerate(layer):
-        admin = admin_lookup(feature.GetField("ADMIN"))
+        admin = admin_names.lookup(feature.GetField("ADMIN"))
         if admin is None:
             continue
         a3 = feature.GetField("SOV_A3")
-        if not admin in df.index:
+        if admin not in df.index:
             df.loc[admin] = [0] * len(df.columns)
 
-        clippedfile = one_feature_shapefile(worldmapname=worldmapname, worldimage=worldimage,
-                a3=a3, idx=idx, feature=feature, tmpdir=tmpdir, srs=srs)
+        clippedfile = one_feature_shapefile(mapfilename=mapfilename, a3=a3, idx=idx,
+                feature=feature, tmpdir=tmpdirobj.name, srs=srs)
         if clippedfile:
             print(f"{admin:<41} #{a3}_{idx}")
-            update_df_from_image(filename=clippedfile, admin=admin,
-                    ctable=ctable, df=df)
+            update_df_from_image(filename=clippedfile, admin=admin, lookupobj=lookupobj, df=df)
         else:
             print(f"{admin:<41} #{a3}_{idx} is empty, skipping.")
 
@@ -213,19 +174,32 @@ def main(shapefilename, worldmapname, tmpdir, csvfilename):
 
 
 if __name__ == '__main__':
-    shapefilename = 'ne_10m_admin_0_countries/ne_10m_admin_0_countries.shp'
+    signal.signal(signal.SIGUSR1, start_pdb)
+    shapefilename = 'data/ne_10m_admin_0_countries/ne_10m_admin_0_countries.shp'
 
-    tmpdirobj = tempfile.TemporaryDirectory()
-    worldmapname = 'Beck_KG_V1/Beck_KG_V1_present_0p0083.tif'
-    csvfilename = 'Köppen-Geiger-present-by-country.csv'
-    print(worldmapname)
-    main(shapefilename=shapefilename, worldmapname=worldmapname, tmpdir=tmpdirobj.name,
-            csvfilename=csvfilename)
+    mapfilename = 'data/ucl_elie/ESACCI-LC-L4-LCCS-Map-300m-P1Y-2015-v2.0.7.tif'
+    csvfilename = 'Land-Cover-by-country.csv'
+    print(mapfilename)
+    lookupobj = LClookup()
+    process_map(shapefilename=shapefilename, mapfilename=mapfilename, lookupobj=lookupobj,
+                csvfilename=csvfilename)
     print('\n')
 
-    tmpdirobj = tempfile.TemporaryDirectory()
-    worldmapname = 'Beck_KG_V1/Beck_KG_V1_future_0p0083.tif'
+    sys.exit(0)
+
+    mapfilename = 'data/Beck_KG_V1/Beck_KG_V1_present_0p0083.tif'
+    csvfilename = 'Köppen-Geiger-present-by-country.csv'
+    print(mapfilename)
+    ctable = osgeo.gdal.Open(mapfilename, osgeo.gdal.GA_ReadOnly).GetRasterBand(1).GetColorTable()
+    lookupobj = KGlookup(ctable)
+    process_map(shapefilename=shapefilename, mapfilename=mapfilename, lookupobj=lookupobj,
+                csvfilename=csvfilename)
+    print('\n')
+
+    mapfilename = 'data/Beck_KG_V1/Beck_KG_V1_future_0p0083.tif'
     csvfilename = 'Köppen-Geiger-future-by-country.csv'
-    print(worldmapname)
-    main(shapefilename=shapefilename, worldmapname=worldmapname, tmpdir=tmpdirobj.name,
-            csvfilename=csvfilename)
+    print(mapfilename)
+    ctable = osgeo.gdal.Open(mapfilename, osgeo.gdal.GA_ReadOnly).GetRasterBand(1).GetColorTable()
+    lookupobj = KGlookup(ctable)
+    process_map(shapefilename=shapefilename, mapfilename=mapfilename, lookupobj=lookupobj,
+                csvfilename=csvfilename)
