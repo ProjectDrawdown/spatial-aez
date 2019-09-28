00a5988679e24c6f3357ef51c0d12468e92dbfad
Denton Gentry <dgentry@carboncaptu.re>
2019-09-28 16:19:33 -0700

    Successfully created sparse GeoTIFF.
    $ python cliptest.py
    Opening data/ucl_elie/ESACCI-LC-L4-LCCS-Map-300m-P1Y-2015-v2.0.7.tif
    opening shapefile
    Creating new shapefile
    Rasterizing
    
    $ ls -l result.tif
    -rw-r--r--  1 dgentry  staff  1444833 Sep 28 15:20 result.tif
    
    $ python cliptest.py
    full: 6685  hole: 122093 partial: 0 error: 0
    
    Note: 2387562 bytes for LZW of a non-sparse file, as the empty
    areas compress really well. Omitting 95% of the blocks reduces
    the file size by 40%. However, the main reason we're doing this
    is not file size, it is so the consumer of these masks can skip
    processing 95% of the blocks altogether.
    
    Note2: As advertised in the documentation, other TIFF-supporting
    software like MacOS Preview cannot grok a sparse file like this.
    It says the file has been damaged. We may make a deliberately
    different filename extension to make this not be so bad.

--- cliptest.py
+++ cliptest.py
@@ -1,22 +1,25 @@
 import os.path
 import time
 
+import numpy as np
 import osgeo.gdal
 import osgeo.ogr
 
 def rasterize_shapefile(img, shapefilename, outfile):
     """Rasterize a shapefile to TIFF."""
-    print('opening shapefile')
-    driver = osgeo.ogr.GetDriverByName("ESRI Shapefile")
-    shapefile = driver.Open(shapefilename, 0)
+    # Step 1: open the original Shapefile.
+    shp_drv = osgeo.ogr.GetDriverByName("ESRI Shapefile")
+    shapefile = shp_drv.Open(shapefilename, 0)
     layer = shapefile.GetLayerByIndex(0)
     srs = layer.GetSpatialRef()
     feature = layer[48]   # Russia
     geom = feature.GetGeometryRef()
     minX, maxX, minY, maxY = geom.GetEnvelope()
+    x_siz = img.RasterXSize
+    y_siz = img.RasterYSize
 
-    print('Creating new shapefile')
-    new_data_source = driver.CreateDataSource('result.shp')
+    # Step 2: extract one feature from the original, and make a new shapefile.
+    new_data_source = shp_drv.CreateDataSource('result.shp')
     new_layer = new_data_source.CreateLayer("feature", geom_type=osgeo.ogr.wkbPolygon, srs=srs)
     new_feat = osgeo.ogr.Feature(layer.GetLayerDefn())
     new_feat.SetGeometry(geom)
@@ -27,19 +30,42 @@ def rasterize_shapefile(img, shapefilename, outfile):
     new_data_source = None
     new_layer = None
 
-    print('Rasterizing')
-    data_source = driver.Open('result.shp', 0)
+    # Step 3: Rasterize the new shapefile to an in-memory buffer.
+    mem_output = osgeo.gdal.GetDriverByName('MEM').Create('', x_siz, y_siz, 1, osgeo.gdal.GDT_Byte)
+    mem_output.SetProjection(img.GetProjection())
+    mem_output.SetGeoTransform(img.GetGeoTransform())
+    data_source = shp_drv.Open('result.shp', 0)
     new_layer = data_source.GetLayer()
+    osgeo.gdal.RasterizeLayer(mem_output, [1], new_layer)
 
-    # Russia (feature #48): DEFLATE: 11 Megs, LZW: 46 Megs.
-    # w/ NBITS=1, Russia ZSTD: 2.3 Megs, DEFLATE: 3.8 Megs, LZW: 15 Megs, PACKBITS: 67 Megs
+    # Step 4: Copy the active pixels to the output file.
+    # The extra copy between steps 3 & 4 is needed to get a Sparse GeoTIFF file, where empty
+    # areas of the raster are omitted from the file entirely. This does reduce the size of the
+    # file, but the main reason for doing it is to allow the subsequent steps using this raster
+    # as a mask to completely skip processing the empty blocks.
     output = osgeo.gdal.GetDriverByName('GTiff').Create(
-            outfile, img.RasterXSize, img.RasterYSize, 1, osgeo.gdal.GDT_Byte,
+            outfile, x_siz, y_siz, 1, osgeo.gdal.GDT_Byte,
+            # ZSTD: 2.3 Megs, DEFLATE: 3.8 Megs, LZW: 15 Megs, PACKBITS: 67 Megs
             options=['NBITS=1', 'COMPRESS=ZSTD', 'TILED=YES', 'NUM_THREADS=2', 'SPARSE_OK=TRUE'])
     output.SetProjection(img.GetProjectionRef())
     output.SetGeoTransform(img.GetGeoTransform())
-    options = osgeo.gdal.RasterizeOptions(outputBounds=[minX, minY, maxX, maxY], outputSRS=srs)
-    osgeo.gdal.RasterizeLayer(output, [1], new_layer)
+
+    # copy the active pixels.
+    x_blksiz = y_blksiz = 256
+    for y_off in range(0, y_siz, y_blksiz):
+        if y_off + y_blksiz < y_siz:
+            rows = y_blksiz
+        else:
+            rows = y_siz - y_off
+        for x_off in range(0, x_siz, x_blksiz):
+            if x_off + x_blksiz < x_siz:
+                cols = x_blksiz
+            else:
+                cols = x_siz - x_off
+            data = mem_output.GetRasterBand(1).ReadAsArray(x_off, y_off, cols, rows)
+            if np.count_nonzero(data) != 0:
+                output.GetRasterBand(1).WriteArray(data, x_off, y_off)
+
 
 
 #for filename in ['data/ucl_elie/ESACCI-LC-L4-LCCS-Map-300m-P1Y-2015-v2.0.7.tif',
@@ -55,7 +81,7 @@ if __name__ == '__main__':
         x_blksiz, y_blksiz = band.GetBlockSize()
         x_siz = band.XSize
         y_siz = band.YSize
-        status_0 = status_1 = status_u = 0
+        status_e = status_p = status_f = status_h = 0
         for y_off in range(0, y_siz, y_blksiz):
             if y_off + y_blksiz < y_siz:
                 rows = y_blksiz
@@ -68,13 +94,16 @@ if __name__ == '__main__':
                     cols = x_siz - x_off
 
                 (flags, pct) = band.GetDataCoverageStatus(x_off, y_off, cols, rows)
-                if flags != osgeo.gdal.GDAL_DATA_COVERAGE_STATUS_DATA:
-                    status_u += 1
-                elif pct == 100.0:
-                    status_1 += 1
+                if flags & osgeo.gdal.GDAL_DATA_COVERAGE_STATUS_EMPTY and pct == 0.0:
+                    status_h += 1
+                elif flags & osgeo.gdal.GDAL_DATA_COVERAGE_STATUS_DATA and pct == 100.0:
+                    status_f += 1
+                elif flags & (osgeo.gdal.GDAL_DATA_COVERAGE_STATUS_EMPTY |
+                        osgeo.gdal.GDAL_DATA_COVERAGE_STATUS_DATA) != 0:
+                    status_p += 1
                 else:
-                    status_0 += 1
-        print(f"0: {status_0}  1: {status_1} unk: {status_u}")
+                    status_e += 1
+        print(f"full: {status_f}  hole: {status_h} partial: {status_p} error: {status_e}")
 
     else:
         filename = 'data/ucl_elie/ESACCI-LC-L4-LCCS-Map-300m-P1Y-2015-v2.0.7.tif'
