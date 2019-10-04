4e0428ddc63af713aa8c5f5d912848219106e3da
Denton Gentry <dgentry@carboncaptu.re>
2019-10-04 12:35:54 -0700

    Increase block size for 4x throughput.
    Other things tried:
    + unpacking the tar files to avoid /vsitar made a small difference
      of ~8%, but we decided the convenience of leaving the tar files was
      worth it.
    + Block sizes of 30 and 100 were both slower. Block sizes of 50 & 60
      were roughly the same. The TIF files are 6000x6000, block needs to
      be an even multiple of 10 and an integer divisor of 6000.

--- data/geomorpho90m/classify_geomorpho90m_slope.py
+++ data/geomorpho90m/classify_geomorpho90m_slope.py
@@ -1,67 +1,55 @@
-import glob
-import tarfile
-
 import numpy as np
 from osgeo import gdal
 
-modelimg = gdal.Open('../Beck_KG_V1/Beck_KG_V1_present_0p0083.tif', gdal.GA_ReadOnly)
-outxsiz = modelimg.RasterXSize
-outysiz = modelimg.RasterYSize
-drv = gdal.GetDriverByName(modelimg.GetDriver().ShortName)
-
-outfilename = 'classified_slope_merit_dem_1km_s0..0cm_2018_v1.0.tif'
-out = drv.Create(outfilename, xsize=outxsiz, ysize=outysiz, bands=8, eType=gdal.GDT_Byte,
-        options = ['NBITS=7', 'COMPRESS=ZSTD', 'TILED=YES', 'NUM_THREADS=2',
-                   'PHOTOMETRIC=MINISBLACK', 'SPARSE_OK=TRUE'])
-out.SetProjection(modelimg.GetProjectionRef())
-out.SetGeoTransform(modelimg.GetGeoTransform())
-out.SetMetadata({
-        'TIFFTAG_ARTIST': 'Derived from work by Giuseppe Amatulli (giuseppe.amatulli@gmail.com)',
-        'TIFFTAG_DATETIME': '2019',
-        'TIFFTAG_IMAGEDESCRIPTION': ('slope classifications, derived from slope geomorpho90m, '
-                                     'itself derived from MERIT-DEM')})
+out = gdal.Open('classified_slope_merit_dem_1km_s0..0cm_2018_v1.0.tif', gdal.GA_Update)
 out_xmin, out_xsiz, _, out_ymin, _, out_ysiz = out.GetGeoTransform()
-outband = {}
-for b in range(1, 9):
-    outband[b] = out.GetRasterBand(b)
-    outband[b].SetNoDataValue(0)
-modelimg = None
 
 f = open('slope_files.txt', 'r')
 for filename in f:
     print(filename.strip())
     slp = gdal.Open(filename.strip(), gdal.GA_ReadOnly)
-    slp_xmin, slp_xsiz, _, slp_ymin, _, slp_ysiz = slp.GetGeoTransform()
-    out_x_initial = int((slp_xmin - out_xmin) / out_xsiz)
-    out_y = int((slp_ymin - out_ymin) / out_ysiz)
     slp_band = slp.GetRasterBand(1)
     slp_x_siz = slp.RasterXSize
     slp_y_siz = slp.RasterYSize
-    slp_x_blksiz = slp_y_blksiz = 10
+    slp_x_blksiz = slp_y_blksiz = 60
+
+    shape = (int(slp_x_siz/10), int(slp_y_siz/10))
+    outband1 = np.empty(shape, dtype=np.uint8)
+    outband2 = np.empty(shape, dtype=np.uint8)
+    outband3 = np.empty(shape, dtype=np.uint8)
+    outband4 = np.empty(shape, dtype=np.uint8)
+    outband5 = np.empty(shape, dtype=np.uint8)
+    outband6 = np.empty(shape, dtype=np.uint8)
+    outband7 = np.empty(shape, dtype=np.uint8)
+    outband8 = np.empty(shape, dtype=np.uint8)
 
     for slp_y in range(0, slp_y_siz, slp_y_blksiz):
-        out_x = out_x_initial
+        out_y = int(slp_y / 10)
         for slp_x in range(0, slp_x_siz, slp_x_blksiz):
             data = slp_band.ReadAsArray(slp_x, slp_y, slp_x_blksiz, slp_y_blksiz)
-            c1 = np.array([[np.sum(np.logical_and(data >= 0, data < 50))]], dtype=np.uint8)
-            outband[1].WriteArray(c1, xoff=out_x, yoff=out_y)
-            c2 = np.array([[np.sum(np.logical_and(data >= 50, data < 200))]], dtype=np.uint8)
-            outband[2].WriteArray(c2, xoff=out_x, yoff=out_y)
-            c3 = np.array([[np.sum(np.logical_and(data >= 200, data < 500))]], dtype=np.uint8)
-            outband[3].WriteArray(c3, xoff=out_x, yoff=out_y)
-            c4 = np.array([[np.sum(np.logical_and(data >= 500, data < 800))]], dtype=np.uint8)
-            outband[4].WriteArray(c4, xoff=out_x, yoff=out_y)
-            c5 = np.array([[np.sum(np.logical_and(data >= 800, data < 1600))]], dtype=np.uint8)
-            outband[5].WriteArray(c5, xoff=out_x, yoff=out_y)
-            c6 = np.array([[np.sum(np.logical_and(data >= 1600, data < 3000))]], dtype=np.uint8)
-            outband[6].WriteArray(c6, xoff=out_x, yoff=out_y)
-            c7 = np.array([[np.sum(np.logical_and(data >= 3000, data < 4500))]], dtype=np.uint8)
-            outband[7].WriteArray(c7, xoff=out_x, yoff=out_y)
-            c8 = np.array([[np.sum(data >= 4500)]])
-            outband[8].WriteArray(c8, xoff=out_x, yoff=out_y)
-            out_x += 1
-        out_y += 1
+            for m in range(slp_x, int(slp_x_blksiz/10)):
+                out_x = int(m / 10)
+                mdata = data[m:m+10, slp_y:slp_y+10]
+                outband1[out_x, out_y] = np.sum(np.logical_and(mdata >= 0, mdata < 50))
+                outband2[out_x, out_y] = np.sum(np.logical_and(mdata >= 50, mdata < 200))
+                outband3[out_x, out_y] = np.sum(np.logical_and(mdata >= 200, mdata < 500))
+                outband4[out_x, out_y] = np.sum(np.logical_and(mdata >= 500, mdata < 800))
+                outband5[out_x, out_y] = np.sum(np.logical_and(mdata >= 800, mdata < 1600))
+                outband6[out_x, out_y] = np.sum(np.logical_and(mdata >= 1600, mdata < 3000))
+                outband7[out_x, out_y] = np.sum(np.logical_and(mdata >= 3000, mdata < 4500))
+                outband8[out_x, out_y] = np.array([[np.sum(mdata >= 4500)]])
 
-for b in range(1, 9):
-    outband[b] = None
+    slp_xmin, _, _, slp_ymin, _, _ = slp.GetGeoTransform()
+    out_x = int((slp_xmin - out_xmin) / out_xsiz)
+    out_y = int((slp_ymin - out_ymin) / out_ysiz)
+
+    out.GetRasterBand(1).WriteArray(outband1, xoff=out_x, yoff=out_y)
+    out.GetRasterBand(2).WriteArray(outband2, xoff=out_x, yoff=out_y)
+    out.GetRasterBand(3).WriteArray(outband3, xoff=out_x, yoff=out_y)
+    out.GetRasterBand(4).WriteArray(outband4, xoff=out_x, yoff=out_y)
+    out.GetRasterBand(5).WriteArray(outband5, xoff=out_x, yoff=out_y)
+    out.GetRasterBand(6).WriteArray(outband6, xoff=out_x, yoff=out_y)
+    out.GetRasterBand(7).WriteArray(outband7, xoff=out_x, yoff=out_y)
+    out.GetRasterBand(8).WriteArray(outband8, xoff=out_x, yoff=out_y)
+
 out = None
